c4cf1923bb6c6063c3087797c18e89b2fa72123f
hyperion9913 <supernoob7903@gmail.com>
2024-08-22 20:25:51 +0530

    ok

--- test.java
+++ test.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class test{
     public static void main(String[] args){
-        System.out.println("Testing...");
+            Scanner sc = new Scanner(System.in);
+            Scanner s = new Scanner(System.in);
+//            int age = sc.nextInt();
+//        System.out.println("Age:"+ age);
+        System.out.println("Your Name?");
+        String name = s.next();
+        System.out.println(name);
+
+        sc.close();
     }
 }
